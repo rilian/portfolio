dd9da05f158ee67d2e556a59fa53abc9339aec76
rilian <dmitriyis@gmail.com>
2012-03-10 16:08:15 +0200

    added user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,29 @@
 FactoryGirl.define do
+  sequence :email do |n|
+    "email.#{n+1}@example.com"
+  end
+
+  sequence :username do |n|
+    "bob_jack_#{n}"
+  end
+
+  sequence :name do |n|
+    "bob_#{n}"
+  end
+
   sequence :title do |n|
     "Title_#{n}"
   end
 
   sequence :body do |n|
     "Body_#{n}"
+  end
+
+  factory :user do
+    username   { Factory.next(:username) }
+    email      { Factory.next(:email) }
+    first_name { Factory.next(:name) }
+    last_name  { Factory.next(:name) }
   end
 
   factory :category do
