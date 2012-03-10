3de58d7801351706916a5e90ddc14c264cc83246
rilian <dmitriyis@gmail.com>
2012-03-10 21:02:23 +0200

    fixed routing spec

--- spec/routing/images_routing_spec.rb
+++ spec/routing/images_routing_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe ImagesController do
   describe "routing" do
     it "recognizes and generates CRUD" do
-      { :put => "/images/1" }.should route_to(:controller => "images", :action => "update", :id => '1')
+      { :post => "/images" }.should route_to(:controller => "images", :action => "create")
       { :delete => "/images/1" }.should route_to(:controller => "images", :action => "destroy", :id => '1')
     end
   end
