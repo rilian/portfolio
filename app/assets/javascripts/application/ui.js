db5fb4cf25a1731f57d4d34a70e512ce95eb8eac
rilian <dmitriyis@gmail.com>
2012-03-18 17:47:21 +0200

    fancybox on body links [ci skip]

--- app/assets/javascripts/application/ui.js
+++ app/assets/javascripts/application/ui.js
@@ -2,6 +2,10 @@ var assign_fancyboxes = function(){
   $("a.fancybox").fancybox({
       'hideOnContentClick': true
   });
+
+  $('#main .post .body a').fancybox({
+    'hideOnContentClick': true
+  });
 };
 
 $(document).ready(function() {
