96676e8fa3c8a66ff9b1952ff5fcf67405318624
rilian <dmitriyis@gmail.com>
2012-04-25 23:31:30 +0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,21 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+
+  validates_presence_of :password, :password_confirmation
+  validates_length_of :password, :minimum => 6
+
+  validates :email, :presence   => true,
+                    :format     => { :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    :uniqueness => { :case_sensitive => false }
+
+  validate :passwords_match
+
+private
+  def passwords_match
+    if self.password != self.password_confirmation
+      self.errors.add(:base, 'password confirmation should match password') and return false
+    end
+    true
+  end
 end
