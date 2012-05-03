dd6df7124956616b8860ecfbffe1910e55b11fd1
rilian <dmitriyis@gmail.com>
2012-05-03 23:26:34 +0300

    better captions when empty

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,23 +1,18 @@
 module ImagesHelper
   def render_image_data(image)
     data = ''
-    if image.title
-      data << "<strong>#{image.title}</strong>"
-      data = data.html_safe
-    end
-    if image.desc
+    if image.desc.present?
       data << ', ' if data.length > 0
       data << image.desc
     end
-    if image.place
+    if image.place.present?
       data << ', ' if data.length > 0
       data << image.place
     end
-    if image.date
+    if image.date.present?
       data << ', ' if data.length > 0
       data << image.date.strftime("%Y")
     end
-    data = 'Untitled' if data == ''
     data
   end
 end
