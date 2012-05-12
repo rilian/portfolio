5dfe09fab7d97f5c49cc98aed6d81686b9bf7938
rilian <dmitriyis@gmail.com>
2012-05-12 14:06:18 +0300

    tags cloud for gallery partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  ##
+  # Returns array of weighted tags, extracted from all Image
+  #
+  def get_tags_cloud
+    tags_cloud = Hash.new(0)
+    Image.published.map(&:tags).flatten.each { |tag| tags_cloud[tag] += 1 }
+    tags_cloud.sort {|a, b| rand <=> rand }
+  end
 end
