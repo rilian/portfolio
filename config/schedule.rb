1048bf2f5da4cbe921b5a5dfa3a4764ca7037d68
rilian <dmitriyis@gmail.com>
2012-12-03 09:22:50 +0200

    temporary disable cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,22 +19,23 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every 30.minutes do
-  rake 'images:publish_unpublished'
-end
+#every 30.minutes do
+#  rake 'images:publish_unpublished'
+#end
 
-every 6.hours do
-  rake 'flickraw:upload_images'
-end
+#every 6.hours do
+#  rake 'flickraw:upload_images'
+#end
 
-every 1.hours do
-  rake 'flickraw:update_images_data'
-  end
+#every 1.hours do
+#  rake 'flickraw:update_images_data'
+#end
 
-every 2.hours do
-  rake 'flickraw:post_comments_to_disqus'
-end
+#every 2.hours do
+#  rake 'flickraw:post_comments_to_disqus'
+#end
 
-every 1.days do
-  rake 'flickraw:remove_deleted_on_site'
-end
+#every 1.days do
+#  rake 'flickraw:remove_deleted_on_site'
+#end
+
