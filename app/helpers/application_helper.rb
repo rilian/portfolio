2d9c36d39139b0fa98d6e6b7f68f1433e01c6d7f
rilian <dmitriyis@gmail.com>
2013-06-08 15:00:40 +0300

    load tags only from last 50 pictures

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   #
   def get_tags_cloud
     tags_cloud = Hash.new(0)
-    Image.published.map(&:tags).flatten.each { |tag| tags_cloud[tag] += 1 }
+    Image.published.limit(50).map(&:tags).flatten.each { |tag| tags_cloud[tag] += 1 }
     tags_cloud.sort_by { rand }
   end
 end
