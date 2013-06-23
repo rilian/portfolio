1804ad18ef150b2f8e9efa87c002f09207a81eed
rilian <dmitriyis@gmail.com>
2013-06-24 01:56:49 +0300

    events index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,11 @@ class EventsController < ApplicationController
   load_resource :event
 
   def index
-    @events = @events.includes([:images])
+    @events = @events.unscoped if params[:q] && params[:q][:s]
+    @q = @events.search(params[:q])
+    @events = @q.result
   end
 
   def show
-    @images = @event.images.published.page(params[:page]).per(Image::PER_PAGE)
   end
 end
