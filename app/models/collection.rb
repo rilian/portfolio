d4ddbee8dbfa225677fcf844677b48bb2d97a3db
rilian <dmitriyis@gmail.com>
2013-07-08 11:39:35 +0300

    collection has no type now

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -4,7 +4,6 @@ class Collection < ActiveRecord::Base
   # Before, after callbacks
 
   # Default scopes, default values (e.g. self.per_page =)
-  TYPES = %w[Album]
 
   # Associations: belongs_to > has_one > has_many > has_and_belongs_to_many
   has_many :images, dependent: :destroy
@@ -12,10 +11,9 @@ class Collection < ActiveRecord::Base
   # Validations: presence > by type > validates
   validates_presence_of :title
   validates_uniqueness_of :title
-  #TODO: validate type by TYPE
 
   # Other properties (e.g. accepts_nested_attributes_for)
-  attr_accessible :type, :title, :is_hidden, :weight, :is_upload_to_stock, :description
+  attr_accessible :title, :is_hidden, :weight, :is_upload_to_stock, :description
 
   # Model dictionaries, state machine
 
@@ -28,13 +26,6 @@ class Collection < ActiveRecord::Base
   # Other model methods
   def to_param
     "#{self.id}-#{self.title.parameterize}"
-  end
-
-  ##
-  # Returns item label in select
-  #
-  def to_label
-    "[#{self.type}] #{self.title}"
   end
 
   # Private methods (for example: custom validators)
