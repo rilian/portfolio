894e002424f211a21245488da14e9df0bff60618
rilian <dmitriyis@gmail.com>
2013-07-09 17:58:33 +0300

    fixign specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -16,9 +16,9 @@ describe HomeController do
 
   describe "GET 'rss'" do
     it 'should be successful' do
-      get :index, format: :rss
+      get :rss, format: :rss
       response.should be_success
-      response.should render_template(:index)
+      response.should render_template('home/rss')
       response.content_type.should eq('application/rss+xml')
     end
   end
