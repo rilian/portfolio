6c80045f664f1bcd0d455e4874e81f42d89c4174
rilian <dmitriyis@gmail.com>
2013-07-14 01:49:58 +0300

    setting default locale; ui to change locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options={})
+    { locale: I18n.locale }
+  end
+
   protect_from_forgery
 
   rescue_from CanCan::AccessDenied do |exception|
