8044722dd93f1e19274920278824dfc5dc6c31f2
rilian <dmitriyis@gmail.com>
2013-07-15 16:38:06 +0300

    validate project title uniqueness

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,6 +12,7 @@ class Project < ActiveRecord::Base
 
   # Validations: presence > by type > validates
   validates_presence_of :title, :info, :description, :title_ua, :info_ua, :description_ua
+  validates_uniqueness_of :title, :title_ua
 
   # Other properties (e.g. accepts_nested_attributes_for)
   attr_accessible :title, :title_ua, :is_published, :description, :description_ua, :weight, :info, :info_ua,
