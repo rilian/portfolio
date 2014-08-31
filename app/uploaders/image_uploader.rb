3fbb8e2a8780e54f1a31e8d7e7acaf5280cd1eac
rilian <dmitriyis@gmail.com>
2014-08-30 23:38:14 -0400

    cleanup uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,37 +3,18 @@
 require 'carrierwave/processing/mime_types'
 
 class ImageUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   include CarrierWave::MimeTypes
   include CarrierWave::ImageOptimizer
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   "/images/fallback/" + [version_name, 'default.png'].compact.join('_')
-  # end
-
-  # Process files as they are uploaded:
-  # process scale: [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
   process :get_version_dimensions
 
-  # Create different versions of your uploaded files:
   version :big do
     process resize_to_limit: [900, 700]
     process :add_watermark
@@ -44,17 +25,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     process optimize: [{ quiet: true }]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  #def filename
-  #  super.chomp(File.extname(super)) + '.png'
-  #end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
 protected
 
