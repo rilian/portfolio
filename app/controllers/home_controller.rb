44b4612a266946e2c0b8488a60d0330fe6b6c40f
rilian <dmitriyis@gmail.com>
2015-05-04 01:19:06 -0400

    show intro screen

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,17 +1,9 @@
 class HomeController < ApplicationController
   def index
-    if params[:q] && params[:q].has_key?('anything_like')
-      params[:q][Image::DEFAULT_QUERY.to_sym] = params[:q].delete('anything_like')
-    end
+    @image = Image.find(ENV['TITLE_IMAGE_ID'])
+    @link = album_path(Album.all.order("title='Portrait'").first)
 
-    @q = Image.from_published_albums.published
-
-    portrait_album = Album.where(title: 'Portrait').first
-    @q = @q.where(album_id: portrait_album.id) if portrait_album
-
-    @q = @q.sorted.includes([:image_tags, :tags]).search(params[:q])
-
-    @images = @q.result(distinct: true).page(params[:page])
+    render layout: nil
   end
 
   def contacts
